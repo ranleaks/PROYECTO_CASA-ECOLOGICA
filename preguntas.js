d499ba6beeba792dae652bf91ade6316b4db86da
ranleaks <162485826+ranleaks@users.noreply.github.com>
2024-06-05 20:30:14 -0700

    Add files via upload

---
+++ preguntas.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+    numb: 1,
+    question: "¿Cuáles son algunas características de una casa ecológica?",
+    answer:"Uso de materiales sostenibles y reciclados en su construcción",
+    options: [
+  "Uso de materiales sostenibles y reciclados en su construcción",
+  "Dependencia exclusiva de energía generada a partir de combustibles fósiles",
+  "Diseño que maximiza la eficiencia energética y la luz natural",
+  "Minimización del uso de tecnologías inteligentes para reducir costos de construcción",
+  ]
+},
+  {
+    numb: 2,
+    question: ". ¿De que materiales se realizo?",
+    answer: "palos de madera, engrudo, carton, pegamento",
+    options: [
+        "palos de madera, engrudo, carton, pegamento",
+        "yeso, palos de madera, engrudo, madera",
+        "cemento, palos de madera, engrudo, yeso, cartulina",
+        "Sign Language HTML"
+  ]
+},
+  {
+    numb: 3,
+    question: ". ¿Cual fue el tiempo de elaboracion?",
+    answer: "3 semanas",
+    options: [
+      "3 Días",
+      "5 Minutos",
+      "3 Semanas",
+      "2 Horas"
+  ]
+},
+  {
+    numb: 4,
+    question: "¿Que generan las casas ecologicas?",
+    answer: "Reduccion del gasto energetico",
+    options: [
+      "Reduccion del agua",
+      "Reduccion del gasto energetico",
+      "Menos contaminacion",
+      "Ninguna"
+  ]
+},
+  {
+  numb: 5,
+  question: "¿CUAL ES EL PROPÓSITO PRINCIPAL DE LA CASA ECOLÓGICA?",
+  answer: "REDUCIR A LA MINIMA EXPRESION EL GASTO ENERGETICO",
+  options: [
+    "REDUCIR EL COSTO DE ENERGÍA Y AYUDAR AL MEDIO AMBIENTE",
+    "MINIMIZAR EL IMPACTO AMBIENTAL",
+    "REDUCIR A LA MINIMA EXPRESION EL GASTO ENERGETICO",
+    "TODAS LAS ANTERIORES"
+  ]
+},
+];
